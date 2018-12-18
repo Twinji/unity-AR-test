43549eefed00c8b6b878c2ffaf5ff43c91148303
Vishnu Gunapathi <38277626+Twinji@users.noreply.github.com>
2018-12-18 22:40:27 +1030

    Add rotational movement to bottle interface and change scene

---
+++ Unity AR Test/Assets/Scripts/BottleUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleUI : MonoBehaviour {
+
+    public float rotationRate = 2f;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(Vector3.up, rotationRate);
+    }
+}
